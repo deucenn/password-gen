8825e7350421e671c0128df39c11c1efedd52814
deucenn <panix.black@web.de>
2023-11-23 18:31:20 +0100

    cleaned up

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 // Slider Value Change
-const sliderElement = document.querySelector(".slider")
-const sliderValue = document.querySelector(".value")
+const sliderElement = document.querySelector(".slider");
+const sliderValue = document.querySelector(".value");
 
 sliderElement.addEventListener("input", (event) => {
   const tempSliderValue = event.target.value;
   sliderValue.textContent = tempSliderValue;
 
   const progress = (tempSliderValue / sliderElement.max) * 100;
-
 
   return progress;
 });
